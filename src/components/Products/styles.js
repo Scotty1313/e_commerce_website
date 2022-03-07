52fdc111ee30697240ece0ffb62c6807c9a539a9
Scotty1313 <63591088+Scotty1313@users.noreply.github.com>
2022-03-07 11:23:44 +0100

    added gradient background

--- src/components/Products/styles.js
+++ src/components/Products/styles.js
@@ -4,7 +4,8 @@ export default makeStyles((theme) => ({
   toolbar: theme.mixins.toolbar,
   content: {
     flexGrow: 1,
-    backgroundColor: theme.palette.background.default,
+    backgroundColor: 'rgb(2,0,36)',
+    background: 'linear-gradient(139deg, rgba(2,0,36,1) 40%, rgba(9,9,121,1) 80%, rgba(29,8,154,1) 100%)',
     padding: theme.spacing(3),
   },
   root: {
